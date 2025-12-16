065f0ad596aae0219371993e9cbee876fe80ae74
trytocode <annguyendang.17.07.2002@gmail.com>
2025-12-16 14:02:43 +0700

    an

--- case_study_module_6/src/main/java/org/example/case_study_module_6/dto/EmployeeDTO.java
+++ case_study_module_6/src/main/java/org/example/case_study_module_6/dto/EmployeeDTO.java
@@ -1,4 +1,21 @@
 package org.example.case_study_module_6.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class EmployeeDTO {
+    private Long id;
+    private String fullName;
+    private String address;
+    private String phoneNumber;
+    private String email;
+    private LocalDate DOB;
 }
