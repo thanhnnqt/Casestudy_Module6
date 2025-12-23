62a3fb20d5cb26d19041bc38eaf3d6f1a913a572
trytocode <annguyendang.17.07.2002@gmail.com>
2025-12-23 16:45:24 +0700

    an

--- case_study_module_6_front_end/src/router/AppRouter.jsx
+++ case_study_module_6_front_end/src/router/AppRouter.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import {Routes, Route} from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
 import Home from "../pages/Home";
 import Login from "../pages/Login.jsx";
@@ -8,29 +8,37 @@ import Profile from "../pages/Profile.jsx";
 import FlightList from "../modules/flight/./components/FlightList.jsx";
 import FlightForm from "../modules/flight/./components/FlightForm.jsx";
 import React from "react";
+import EmployeeList from "../modules/employee/components/EmployeeList.jsx";
+import EmployeeDetail from "../modules/employee/components/EmployeeDetail.jsx";
+import AddEmployee from "../modules/employee/components/AddEmployee.jsx";
 
 function AppRouter() {
     return (
         <Routes>
-            <Route element={<MainLayout />}>
+            <Route element={<MainLayout/>}>
 
                 {/*Route trang chủ*/}
-                <Route path="/" element={<Home />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
+                <Route path="/" element={<Home/>}/>
+                <Route path="/login" element={<Login/>}/>
+                <Route path="/register" element={<Register/>}/>
 
 
                 {/*Route chuyến bay*/}
-                <Route path="/flights" element={<FlightList />} />
-                <Route path="/flights/create" element={<FlightForm />} />
-                <Route path="flights/edit/:id" element={<FlightForm />} />
+                <Route path="/flights" element={<FlightList/>}/>
+                <Route path="/flights/create" element={<FlightForm/>}/>
+                <Route path="flights/edit/:id" element={<FlightForm/>}/>
 
+
+                {/*Route nhân viên*/}
+                <Route path="/employees" element={<EmployeeList/>}/>
+                <Route path="/employees/edit/:id" element={<EmployeeDetail/>}/>
+                <Route path="/employees/add" element={<AddEmployee/>}/>
 
                 <Route
                     path="/profile"
                     element={
                         <PrivateRoute>
-                            <Profile />
+                            <Profile/>
                         </PrivateRoute>
                     }
                 />
